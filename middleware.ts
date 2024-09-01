a1527abbf22464095c82aa4959b129b4b44ee395
anuar <a.shaidenov@aix.kz>
2024-09-01 22:17:58 +0500

    Change default locale

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,16 @@
-import createMiddleware from 'next-intl/middleware';
+import createMiddleware from "next-intl/middleware";
 
-import { locales, localePrefix } from './navigation';
+import { locales, localePrefix } from "./navigation";
 
 export default createMiddleware({
   // A list of all locales that are supported
   locales,
   localePrefix,
   // Used when no locale matches
-  defaultLocale: 'en',
+  defaultLocale: "kz",
 });
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(kz|en)/:path*'],
+  matcher: ["/", "/(kz|en)/:path*"],
 };
