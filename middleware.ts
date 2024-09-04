9d998e42e4047ff68399557ef5fb64bf47003326
anuar <a.shaidenov@aix.kz>
2024-09-04 21:27:55 +0500

    Add supabase configs

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,28 @@
 import createMiddleware from "next-intl/middleware";
 
 import { locales, localePrefix } from "./navigation";
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware";
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales,
-  localePrefix,
-  // Used when no locale matches
-  defaultLocale: "kz",
-});
+export async function middleware(request: NextRequest) {
+  const handleI18nRouting = createMiddleware({
+    // A list of all locales that are supported
+    locales,
+    localePrefix,
+    // Used when no locale matches
+    defaultLocale: "kz",
+  });
+  const res = handleI18nRouting(request);
+
+  // update user's auth session
+  return await updateSession(request, res);
+}
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(kz|en)/:path*"],
+  matcher: [
+    "/",
+    "/(kz|en)/:path*",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
