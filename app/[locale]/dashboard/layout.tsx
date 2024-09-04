e9b53321ace14d6c709aab37f9f9238d57aebdf2
anuar <a.shaidenov@aix.kz>
2024-09-04 23:46:03 +0500

    init dahsboard

---
+++ app/[locale]/dashboard/layout.tsx
@@ -0,0 +1,104 @@
+import { Home, LifeBuoy, LogOut, Settings2, SquareUser } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Link } from "@/navigation";
+import { signOut } from "@/actions/sign-out";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const DashboardLayout = ({ children }: Props) => {
+  return (
+    <TooltipProvider>
+      <div className="grid h-screen w-full pl-[53px]">
+        <aside className="inset-y fixed  left-0 z-20 flex h-full flex-col border-r">
+          <div className="border-b p-2">
+            <Button variant="outline" size="icon" aria-label="Home" asChild>
+              <Link href={"/"}>💸</Link>
+            </Button>
+          </div>
+          <nav className="grid gap-1 p-2">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="rounded-lg"
+                  aria-label="Settings"
+                >
+                  <Home className="size-5" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="right" sideOffset={5}>
+                Home
+              </TooltipContent>
+            </Tooltip>
+            {/* <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="rounded-lg"
+                  aria-label="Settings"
+                >
+                  <Settings2 className="size-5" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="right" sideOffset={5}>
+                Settings
+              </TooltipContent>
+            </Tooltip> */}
+          </nav>
+          <nav className="mt-auto grid gap-1 p-2">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <form action={signOut}>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="mt-auto rounded-lg"
+                    aria-label="Help"
+                  >
+                    <LogOut className="size-5" />
+                  </Button>
+                </form>
+              </TooltipTrigger>
+              <TooltipContent side="right" sideOffset={5}>
+                Logout
+              </TooltipContent>
+            </Tooltip>
+            {/* <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="mt-auto rounded-lg"
+                  aria-label="Account"
+                >
+                  <SquareUser className="size-5" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="right" sideOffset={5}>
+                Account
+              </TooltipContent>
+            </Tooltip> */}
+          </nav>
+        </aside>
+        <div className="flex flex-col">
+          <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
+            <h1 className="text-xl font-semibold">Qarjy</h1>
+          </header>
+          <main className="grid flex-1 overflow-auto">{children}</main>
+        </div>
+      </div>
+    </TooltipProvider>
+  );
+};
+
+export default DashboardLayout;
--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -1,9 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { getTranslations } from "next-intl/server";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
-function DashboardPage({}: Props) {
-  return <div>DashboardPage</div>;
+async function DashboardPage({}: Props) {
+  const t = await getTranslations();
+  return (
+    <section className="container py-20">
+      <div className="flex text-center items-center flex-col gap-4">
+        <h1 className="md:text-5xl text-lg font-mono">
+          {t("dashboard.home.title")}
+        </h1>
+        <Button asChild size={"lg"}>
+          <Link href={"/"}>{t("dashboard.home.button")}</Link>
+        </Button>
+      </div>
+    </section>
+  );
 }
 
 export default DashboardPage;
