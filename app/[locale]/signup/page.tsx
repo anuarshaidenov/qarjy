19ab28336e9ddedbf1ed02f36390c1cb3e34584f
anuar <a.shaidenov@aix.kz>
2024-09-05 11:19:29 +0500

    Update locales

--- app/[locale]/signup/page.tsx
+++ app/[locale]/signup/page.tsx
@@ -1,4 +1,5 @@
 import { signUpWithGoogle } from "@/actions/signup-with-google";
+import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { Link } from "@/navigation";
 import { getTranslations } from "next-intl/server";
@@ -12,11 +13,12 @@ const LoginPage = async (props: Props) => {
   return (
     <main className="h-screen">
       <div className="container flex flex-col items-center justify-center gap-8 h-full w-full">
-        <h1 className="text-5xl font-bold">{t("signup.title")}</h1>
+        <Logo className="text-xl md:text-3xl mr-2" />
+        <h1 className="text-5xl md:text-8xl font-bold">{t("signup.title")}</h1>
 
         <form className="flex flex-col gap-4" action={signUpWithGoogle}>
-          <Button>{t("signup.button")}</Button>
-          <Button variant={"secondary"} asChild>
+          <Button> {t("signup.button")}</Button>
+          <Button variant={"outline"} asChild>
             <Link href={"/"}>{t("signup.back-button")}</Link>
           </Button>
         </form>
