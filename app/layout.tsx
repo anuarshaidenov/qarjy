e30059bc9e43d25c2691c51769382fc7e3b61838
anuar <anuarshaidenov@gmail.com>
2024-09-06 14:48:05 +0500

    Revert "Try metadata generation from main layout"
    This reverts commit dede5e94a0fdd7e02bea55ed5656b7e4ad346dbc.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,21 @@
 import { ReactNode } from "react";
 import "./globals.css";
 import { Metadata } from "next";
-import { getTranslations } from "next-intl/server";
 
 type Props = {
   children: ReactNode;
 };
 
-export async function generateMetadata({
-  params,
-}: Readonly<{ params: { locale: string } }>) {
-  const t = await getTranslations({
-    locale: params.locale,
-  });
-
-  return {
-    title: t("metadata.title"),
-    description: t("metadata.description"),
-    authors: [
-      {
-        name: "Anuar Shaidenov",
-        url: "https://anuarshaidenov.vercel.app/",
-      },
-    ],
-    openGraph: {
-      title: t("metadata.openGraph.title"),
-      description: t("metadata.openGraph.description"),
-      url: t("metadata.openGraph.url"),
-      siteName: t("metadata.openGraph.siteName"),
-      images: [
-        {
-          url: "og-image.png",
-          width: 1200,
-          height: 630,
-          alt: "Qarjy app screen",
-        },
-      ],
-      locale: params.locale,
-      type: "website",
+export const metadata: Metadata = {
+  metadataBase: new URL("https://qarjy.site"),
+  alternates: {
+    canonical: "/",
+    languages: {
+      kk: "/kz",
+      en: "/en",
     },
-    twitter: {
-      card: "summary_large_image",
-      title: t("metadata.twitter.title"),
-      description: t("metadata.twitter.description"),
-      creator: "@anuarnyi",
-      images: ["og-image.png"],
-    },
-    robots: "index, follow",
-  } as Metadata;
-}
+  },
+};
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
