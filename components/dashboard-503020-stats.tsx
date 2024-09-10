cdef74846ce486efa1bd69cae23ed8bd2c89ef2d
anuar <anuarshaidenov@gmail.com>
2024-09-10 11:23:11 +0500

    Update copies

--- components/dashboard-503020-stats.tsx
+++ components/dashboard-503020-stats.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { ExpensesPieChart } from './expenses-pie-chart';
+"use client";
+
+import { ExpensesPieChart } from "./expenses-pie-chart";
+import { useTranslations } from "next-intl";
 
 type Props = {};
 
 export const Dashboard503020Stats = (props: Props) => {
+  const t = useTranslations();
+
   return (
     <div className="flex flex-col gap-8">
-      <ExpensesPieChart expenseType="essential" title="Essential expenses" />
+      <ExpensesPieChart
+        expenseType="essential"
+        title={t("dashboard.app.stats.essential-expenses-title")}
+      />
       <ExpensesPieChart
         expenseType="non-essential"
-        title="Non-essential expenses"
+        title={t("dashboard.app.stats.non-essential-expenses-title")}
       />
     </div>
   );
