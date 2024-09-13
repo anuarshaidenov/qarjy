e283e5fcc3d7c235453e5e61fe81d4f41db9e9dc
anuarshaidenov <anuarshaidenov@gmail.com>
2024-09-13 20:33:39 +0500

    Add metadata to the landing

--- app/[locale]/(landing)/layout.tsx
+++ app/[locale]/(landing)/layout.tsx
@@ -1,9 +1,54 @@
-import { Footer } from "@/components/footer";
-import { Header } from "@/components/header";
+import { Footer } from '@/components/footer';
+import { Header } from '@/components/header';
+import { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
 
 type Props = {
   children: React.ReactNode;
 };
+
+export async function generateMetadata({
+  params,
+}: Readonly<{ params: { locale: string } }>) {
+  const t = await getTranslations({
+    locale: params.locale,
+  });
+
+  return {
+    title: t('metadata.title'),
+    description: t('metadata.description'),
+    authors: [
+      {
+        name: 'Anuar Shaidenov',
+        url: 'https://anuarshaidenov.vercel.app/',
+      },
+    ],
+    openGraph: {
+      title: t('metadata.openGraph.title'),
+      description: t('metadata.openGraph.description'),
+      url: t('metadata.openGraph.url'),
+      siteName: t('metadata.openGraph.siteName'),
+      images: [
+        {
+          url: 'og-image.png',
+          width: 1200,
+          height: 630,
+          alt: 'Qarjy app screen',
+        },
+      ],
+      locale: params.locale === 'kz' ? 'kk' : params.locale,
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: t('metadata.twitter.title'),
+      description: t('metadata.twitter.description'),
+      creator: 'anuarnyi',
+      images: ['og-image.png'],
+    },
+    robots: 'index, follow',
+  } as Metadata;
+}
 
 const LandingLayout = (props: Props) => {
   return (
