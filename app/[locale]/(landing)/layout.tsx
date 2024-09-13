7f30cc190e6054748c74713d127c7f8d0e20ee79
anuarshaidenov <anuarshaidenov@gmail.com>
2024-09-13 22:18:12 +0500

    Add unstable to the layout

--- app/[locale]/(landing)/layout.tsx
+++ app/[locale]/(landing)/layout.tsx
@@ -1,11 +1,15 @@
 import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
+import { unstable_setRequestLocale } from 'next-intl/server';
 
 type Props = {
   children: React.ReactNode;
+  params: { locale: string };
 };
 
 const LandingLayout = (props: Props) => {
+  unstable_setRequestLocale(props.params.locale);
+
   return (
     <>
       <Header />
