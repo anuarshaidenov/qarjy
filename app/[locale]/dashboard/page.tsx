be7fe64037b52925824a973639004c1de5d5e929
anuarshaidenov <anuarshaidenov@gmail.com>
2024-09-13 22:00:29 +0500

    Add static params

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { BudgetCard } from "@/components/budget-card";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useGetBudgets } from "@/hooks/use-get-budgets";
-import { useTranslations } from "next-intl";
-import React from "react";
+import { BudgetCard } from '@/components/budget-card';
+import { Skeleton } from '@/components/ui/skeleton';
+import { useGetBudgets } from '@/hooks/use-get-budgets';
+import { useTranslations } from 'next-intl';
+import React from 'react';
 
 type Props = {};
 
@@ -15,10 +15,10 @@ function DashboardPage({}: Props) {
   return (
     <section className="py-8 px-4 container flex flex-col gap-8">
       <h1 className="text-3xl md:text-5xl font-bold">
-        {t("dashboard.all-budgets.title")}
+        {t('dashboard.all-budgets.title')}
       </h1>
       <p className="text-muted-foreground">
-        {t("dashboard.all-budgets.description")}
+        {t('dashboard.all-budgets.description')}
       </p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8 lg:grid-cols-4">
         {isLoading ? (
--- app/[locale]/signup/page.tsx
+++ app/[locale]/signup/page.tsx
@@ -1,25 +1,28 @@
-import { signUpWithGoogle } from "@/actions/signup-with-google";
-import { Logo } from "@/components/logo";
-import { Button } from "@/components/ui/button";
-import { Link } from "@/navigation";
-import { getTranslations } from "next-intl/server";
-import React from "react";
+import { signUpWithGoogle } from '@/actions/signup-with-google';
+import { Logo } from '@/components/logo';
+import { Button } from '@/components/ui/button';
+import { Link } from '@/navigation';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+import React from 'react';
 
-type Props = {};
+type Props = {
+  params: { locale: string };
+};
 
 const LoginPage = async (props: Props) => {
   const t = await getTranslations();
+  unstable_setRequestLocale(props.params.locale);
 
   return (
     <main className="h-screen">
       <div className="container flex flex-col items-center justify-center gap-8 h-full w-full">
         <Logo className="text-xl md:text-3xl mr-2" />
-        <h1 className="text-5xl md:text-8xl font-bold">{t("signup.title")}</h1>
+        <h1 className="text-5xl md:text-8xl font-bold">{t('signup.title')}</h1>
 
         <form className="flex flex-col gap-4" action={signUpWithGoogle}>
-          <Button> {t("signup.button")}</Button>
-          <Button variant={"outline"} asChild>
-            <Link href={"/"}>{t("signup.back-button")}</Link>
+          <Button> {t('signup.button')}</Button>
+          <Button variant={'outline'} asChild>
+            <Link href={'/'}>{t('signup.back-button')}</Link>
           </Button>
         </form>
       </div>
