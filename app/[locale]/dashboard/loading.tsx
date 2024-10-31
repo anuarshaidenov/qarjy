5d177caf91c74acf8065605d2e7e75c0befcb88e
anuar <anuarshaidenov@gmail.com>
2024-10-31 15:58:46 +0500

    Add loading skeleton

--- app/[locale]/dashboard/loading.tsx
+++ app/[locale]/dashboard/loading.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type Props = {};
 
-function LoadingPage({}: Props) {
-  return null;
+function DashboardLoading({}: Props) {
+  return (
+    <section className="h-full">
+      <div className="py-8 px-4 container flex flex-col gap-4">
+        <Skeleton className="w-full md:w-[385px] h-[36px] md:h-[48px]" />
+
+        <Skeleton className="w-full h-[36px] md:h-[24px]" />
+
+        <Skeleton className="w-full h-[1000px]" />
+      </div>
+    </section>
+  );
 }
 
-export default LoadingPage;
+export default DashboardLoading;
