ea41ca7da3555ff177548ea67fdfb211732f6d84
anuar <anuarshaidenov@gmail.com>
2024-10-31 10:18:14 +0500

    Update metadata for monthly budget

--- app/[locale]/dashboard/monthly-budget/[id]/page.tsx
+++ app/[locale]/dashboard/monthly-budget/[id]/page.tsx
@@ -1,9 +1,21 @@
+import { getBudgetById } from "@/actions/get-budget-by-id";
 import { DashboardApp } from "@/components/dashboard-app";
+import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 
 type Props = {
   params: { id: string };
 };
+
+export async function generateMetadata(props: Props) {
+  const data = await getBudgetById(props.params.id);
+
+  const metadata: Metadata = {
+    title: data?.title,
+  };
+
+  return metadata;
+}
 
 async function MonthlyBudgetPage({ params: { id } }: Props) {
   const t = await getTranslations();
