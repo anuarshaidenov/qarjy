f22b42123fc51d94074d710b27b8ad40bb07724c
anuar <anuarshaidenov@gmail.com>
2024-12-08 01:05:19 +0500

    Update loading states

--- app/[locale]/dashboard/monthly-budget/loading/page.tsx
+++ app/[locale]/dashboard/monthly-budget/loading/page.tsx
@@ -6,10 +6,6 @@ const MonthlyBudgetLoadingPage = (props: Props) => {
   return (
     <section className="h-full">
       <div className="py-8 px-4 container flex flex-col gap-4">
-        <Skeleton className="w-full md:w-[385px] h-[36px] md:h-[48px]" />
-
-        <Skeleton className="w-full h-[36px] md:h-[24px] mb-8" />
-
         <Skeleton className="w-full h-[36px] mb-2" />
         <div className="grid md:grid-cols-2 gap-8">
           <Skeleton className="h-[700px]" />
