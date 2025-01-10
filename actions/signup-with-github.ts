302042b23383cac84f0a8cebde57428f92ffbaec
anuar <anuarshaidenov@gmail.com>
2025-01-10 15:52:48 +0500

    Add github authentication

---
+++ actions/signup-with-github.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function signInWithGithub() {
+  const header = headers();
+  const host = header.get("host");
+  const isLocalEnv = process.env.NODE_ENV === "development";
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+    options: {
+      redirectTo: isLocalEnv
+        ? `http://${host}/auth/callback`
+        : `https://${host}/auth/callback`,
+    },
+  });
+
+  if (data.url) {
+    redirect(data.url); // use the redirect API for your server framework
+  }
+}
--- app/[locale]/signup/page.tsx
+++ app/[locale]/signup/page.tsx
@@ -1,9 +1,12 @@
-import { signUpWithGoogle } from '@/actions/signup-with-google';
-import { Logo } from '@/components/logo';
-import { Button } from '@/components/ui/button';
-import { Link } from '@/navigation';
-import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
-import React from 'react';
+import { signInWithGithub } from "@/actions/signup-with-github";
+import { signUpWithGoogle } from "@/actions/signup-with-google";
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Link } from "@/navigation";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import React from "react";
 
 type Props = {
   params: { locale: string };
@@ -17,17 +20,61 @@ const LoginPage = async (props: Props) => {
     <main className="h-screen">
       <div className="container flex flex-col items-center justify-center gap-8 h-full w-full">
         <Logo className="text-xl md:text-3xl mr-2" />
-        <h1 className="text-5xl md:text-8xl font-bold">{t('signup.title')}</h1>
+        <h1 className="text-5xl md:text-8xl font-bold sr-only">
+          {t("signup.title")}
+        </h1>
 
-        <form className="flex flex-col gap-4" action={signUpWithGoogle}>
-          <Button> {t('signup.button')}</Button>
-          <Button variant={'outline'} asChild>
-            <Link href={'/'}>{t('signup.back-button')}</Link>
+        <div className="flex flex-col gap-4 w-full max-w-xs">
+          <form action={signUpWithGoogle}>
+            <Button className="w-full" variant={"secondary"}>
+              <GoogleLogo />
+              {t("signup.button")}
+            </Button>
+          </form>
+          <form action={signInWithGithub}>
+            <Button className="w-full" variant={"secondary"}>
+              <GitHubLogoIcon className="mr-2 size-4" />
+              {t("signup-github.button")}
+            </Button>
+          </form>
+          <Separator />
+          <Button asChild variant={"outline"}>
+            <Link href={"/"}>{t("signup.back-button")}</Link>
           </Button>
-        </form>
+        </div>
       </div>
     </main>
   );
 };
 
 export default LoginPage;
+
+const GoogleLogo = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      height={16}
+      width={16}
+      className="mr-2"
+    >
+      <path
+        d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+        fill="#4285F4"
+      />
+      <path
+        d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+        fill="#34A853"
+      />
+      <path
+        d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+        fill="#FBBC05"
+      />
+      <path
+        d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+        fill="#EA4335"
+      />
+      <path d="M1 1h22v22H1z" fill="none" />
+    </svg>
+  );
+};
