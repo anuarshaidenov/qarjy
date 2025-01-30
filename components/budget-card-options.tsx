3acb85e0aec0eff8858b8c162eedbc97a77669d4
anuar <anuarshaidenov@gmail.com>
2025-01-30 11:39:38 +0500

    Add confirmation dialog to delete action

--- components/budget-card-options.tsx
+++ components/budget-card-options.tsx
@@ -1,6 +1,18 @@
 "use client";
 
 import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
@@ -26,23 +38,48 @@ export const BudgetCardOptions = (props: Props) => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          size={"icon"}
-          variant={"ghost"}
-          className={cn(props.triggerClassname)}
-        >
-          <Ellipsis className="size-5" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem
-          onClick={handleDelete}
-          className="bg-red-600 hover:bg-red-700 focus:bg-red-700 cursor-pointer text-white hover:text-white focus:text-white flex items-center justify-between"
-        >
-          Delete <Trash className="size-4" />
-        </DropdownMenuItem>
-      </DropdownMenuContent>
+      <AlertDialog>
+        <DropdownMenuTrigger asChild>
+          <Button
+            size={"icon"}
+            variant={"ghost"}
+            className={cn(props.triggerClassname)}
+          >
+            <Ellipsis className="size-5" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            asChild
+            className="bg-red-600 hover:bg-red-700 focus:bg-red-700 cursor-pointer text-white hover:text-white focus:text-white flex items-center justify-between"
+          >
+            <AlertDialogTrigger
+              className="w-full"
+              onClick={(e) => e.stopPropagation()}
+            >
+              Delete <Trash className="size-4" />
+            </AlertDialogTrigger>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+        <AlertDialogContent onClick={(e) => e.stopPropagation()}>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              budget.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={handleDelete}
+              className="bg-red-600 text-foreground hover:bg-red-700"
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </DropdownMenu>
   );
 };
