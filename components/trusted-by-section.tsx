3b335c3fac4fd318164eba71f97b6e380747a87d
anuar <anuarshaidenov@gmail.com>
2025-02-03 16:45:18 +0500

    Add translations to trusted by section

--- components/trusted-by-section.tsx
+++ components/trusted-by-section.tsx
@@ -2,11 +2,13 @@
 
 import { motion, useScroll, useTransform } from "framer-motion";
 import { Pin } from "lucide-react";
+import { useTranslations } from "next-intl";
 import React, { useRef } from "react";
 
 type Props = {};
 
 export const TrustedBySection = (props: Props) => {
+  const t = useTranslations("");
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -25,9 +27,7 @@ export const TrustedBySection = (props: Props) => {
           className="bg-muted relative rounded-[3px] -rotate-6 font-mono py-24 px-12 text-center flex items-center justify-center"
         >
           <Pin className="absolute size-9 -top-[16px] left-1/2 -translate-x-1/2" />
-          <p className="text-xl font-bold">
-            500+ users improved their finances with qarjy.
-          </p>
+          <p className="text-xl font-bold">{t("trusted-by-text")}</p>
         </motion.div>
       </div>
     </section>
