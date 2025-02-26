ac226e7fe9d8782ad650731958e114cfe1d73fd5
anuar <anuarshaidenov@gmail.com>
2025-02-26 11:56:42 +0500

    Add pwa settings

---
+++ app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Qarjy",
+    short_name: "Qarjy",
+    description:
+      "Easily manage your finances with our intuitive budgeting tool. Track your expenses, savings, and investments using flexible methods.",
+    start_url: "/dashboard",
+    display: "standalone",
+    background_color: "#191919",
+    theme_color: "#3b82f6",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
