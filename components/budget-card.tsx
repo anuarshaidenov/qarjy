b2bca3de8d54fa1af994fab5ca59ead731007ad3
anuar <anuarshaidenov@gmail.com>
2025-02-26 19:16:31 +0500

    Prefetch on budget hover

--- components/budget-card.tsx
+++ components/budget-card.tsx
@@ -1,13 +1,16 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
-import { Link } from '@/navigation';
-import { Budget } from '@/types/budget';
-import { cn, formatNumberWithCommas } from '@/lib/utils';
-import { useTranslations } from 'next-intl';
-import { useCurrency } from './currency-provider';
-import { BudgetCardOptions } from './budget-card-options';
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Link } from "@/navigation";
+import { Budget } from "@/types/budget";
+import { cn, formatNumberWithCommas } from "@/lib/utils";
+import { useTranslations } from "next-intl";
+import { useCurrency } from "./currency-provider";
+import { BudgetCardOptions } from "./budget-card-options";
+import { useQueryClient } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import axios from "axios";
 
 type Props = {
   budget: Budget;
@@ -18,13 +21,26 @@ export const BudgetCard = ({ budget }: Props) => {
   const { currency } = useCurrency();
 
   const [loading, setLoading] = React.useState(false);
+  const queryClient = useQueryClient();
+
+  const handleHover = () => {
+    queryClient.prefetchQuery({
+      queryKey: [QUERY_KEYS.BUDGET, budget.id],
+      queryFn: async () => {
+        return axios
+          .get<Budget>(`/api/budgets/${budget.id}`)
+          .then((res) => res.data);
+      },
+    });
+  };
 
   return (
-    <Link href={'/dashboard/budget/' + budget.id}>
+    <Link href={"/dashboard/budget/" + budget.id}>
       <Card
+        onMouseEnter={handleHover}
         className={cn(
-          'hover:border-foreground/50 transition-colors min-h-36 relative',
-          loading && 'animate-pulse'
+          "hover:border-foreground/50 transition-colors min-h-36 relative",
+          loading && "animate-pulse"
         )}
       >
         <CardHeader>
@@ -33,7 +49,7 @@ export const BudgetCard = ({ budget }: Props) => {
         <CardContent className="text-sm">
           <div className="space-y-1">
             <p>
-              {t('budget-card.income')}:{' '}
+              {t("budget-card.income")}:{" "}
               <span className="font-bold">
                 {formatNumberWithCommas(budget.monthlyIncome)} {currency.symbol}
               </span>
