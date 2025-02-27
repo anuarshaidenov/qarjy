771461148929eecd308eeaed49a5a85891aa6b71
anuar <anuarshaidenov@gmail.com>
2025-02-27 09:52:40 +0500

    Prefetch budget data on server

--- app/[locale]/dashboard/budget/[id]/page.tsx
+++ app/[locale]/dashboard/budget/[id]/page.tsx
@@ -1,5 +1,10 @@
 import { getBudgetById } from "@/actions/get-budget-by-id";
+import { getQueryClient } from "@/actions/get-query-client";
 import { DashboardApp } from "@/components/dashboard-app";
+import { QUERY_KEYS } from "@/lib/constants";
+import { Budget } from "@/types/budget";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import axios from "axios";
 import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 
@@ -20,18 +25,26 @@ export async function generateMetadata(props: Props) {
 async function MonthlyBudgetPage(props: Props) {
   const params = await props.params;
 
-  const {
-    id
-  } = params;
+  const { id } = params;
 
   const t = await getTranslations();
 
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: [QUERY_KEYS.BUDGET, id],
+    queryFn: () =>
+      axios.get<Budget>(`/api/budgets/${id}`).then((res) => res.data),
+  });
+
   return (
-    <section className="h-full">
-      <div className="py-8 px-4 container flex flex-col gap-4">
-        <DashboardApp />
-      </div>
-    </section>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <section className="h-full">
+        <div className="py-8 px-4 container flex flex-col gap-4">
+          <DashboardApp />
+        </div>
+      </section>
+    </HydrationBoundary>
   );
 }
 
