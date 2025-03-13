4ec1f0069134317146705442f82b9caa2144c74a
anuar <anuarshaidenov@gmail.com>
2025-03-13 12:51:45 +0500

    Update loading state for create function and add description

--- components/create-budget-dialog.tsx
+++ components/create-budget-dialog.tsx
@@ -1,26 +1,71 @@
-'use client';
+"use client";
 
-import React from 'react';
+import React from "react";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-} from '@/components/ui/dialog';
-import { useCreateBudgetDialog } from './create-budget-dialog-provider';
-import { useToast } from '@/hooks/use-toast';
-import { createBudget } from '@/actions/create-budget';
-import { useRouter } from 'next/navigation';
-import { Card } from './ui/card';
-import { cn } from '@/lib/utils';
-import { useTranslations } from 'next-intl';
-import { BUDGET_TYPES } from '@/lib/constants';
+} from "@/components/ui/dialog";
+import { useCreateBudgetDialog } from "./create-budget-dialog-provider";
+import { useToast } from "@/hooks/use-toast";
+import { createBudget } from "@/actions/create-budget";
+import { useRouter } from "next/navigation";
+import { Card } from "./ui/card";
+import { cn } from "@/lib/utils";
+import { useTranslations } from "next-intl";
+import { BUDGET_TYPES } from "@/lib/constants";
+import { Loader, Loader2 } from "lucide-react";
 
 type Props = {};
 
 export const CreateBudgetDialog = (props: Props) => {
   const { open, setOpen } = useCreateBudgetDialog();
+  const t = useTranslations();
+
+  const description503020 = t("50-30-20-description");
+  const description751015 = t("75-10-15-description");
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader className="mb-4">
+          <DialogTitle>{t("create-budget")}</DialogTitle>
+          <DialogDescription>
+            {t("create-budget-description")}
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid sm:grid-cols-2 gap-4">
+          <CreateBudgetButton
+            type="50-30-20"
+            heading="50-30-20"
+            description={description503020}
+            highlight
+          />
+          <CreateBudgetButton
+            type="75-10-15"
+            heading="75-10-15"
+            description={description751015}
+          />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const CreateBudgetButton = ({
+  heading,
+  description,
+  type,
+  highlight,
+}: {
+  heading?: string;
+  description?: string;
+  type: string;
+  highlight?: boolean;
+}) => {
+  const { setOpen } = useCreateBudgetDialog();
   const [isPending, startTransition] = React.useTransition();
   const { toast } = useToast();
   const router = useRouter();
@@ -32,9 +77,9 @@ export const CreateBudgetDialog = (props: Props) => {
 
       if (error || !data) {
         toast({
-          title: 'Error creating budget',
+          title: "Error creating budget",
           description: error?.message,
-          variant: 'destructive',
+          variant: "destructive",
         });
         return;
       }
@@ -46,41 +91,31 @@ export const CreateBudgetDialog = (props: Props) => {
   };
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader className="mb-4">
-          <DialogTitle>{t('create-budget')}</DialogTitle>
-          <DialogDescription>
-            {t('create-budget-description')}
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid sm:grid-cols-2 gap-4">
-          <button
-            disabled={isPending}
-            className={cn(isPending && 'animate-pulse')}
-            onClick={() => handleClick(BUDGET_TYPES['50-30-20'])}
-          >
-            <Card
-              className={'flex items-center h-full justify-center min-h-36'}
-              aria-label="New Budget"
-            >
-              <span className="font-bold md:text-2xl">50-30-20</span>
-            </Card>
-          </button>
-          <button
-            disabled={isPending}
-            className={cn(isPending && 'animate-pulse')}
-            onClick={() => handleClick('75-10-15')}
-          >
-            <Card
-              className={'flex items-center h-full justify-center min-h-36'}
-              aria-label="New Budget"
-            >
-              <span className="font-bold md:text-2xl">75-10-15</span>
-            </Card>
-          </button>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <button
+      disabled={isPending}
+      className={cn(isPending && "animate-pulse")}
+      onClick={() => handleClick(type)}
+    >
+      <Card
+        className={cn(
+          "flex flex-col p-4 gap-2 items-center h-full justify-center min-h-36 hover:opacity-85",
+          highlight && "border border-green-600 relative"
+        )}
+        aria-label="New Budget"
+      >
+        {highlight && (
+          <span className="absolute -top-5 whitespace-nowrap right-1/2 translate-x-1/2 text-xs text-green-600">
+            {t("most-popular")}
+          </span>
+        )}
+        {!isPending && (
+          <>
+            <span className="font-bold md:text-2xl">{heading}</span>
+            <p className="text-muted-foreground text-xs">{description}</p>
+          </>
+        )}
+        {isPending && <Loader2 className="animate-spin" />}
+      </Card>
+    </button>
   );
 };
