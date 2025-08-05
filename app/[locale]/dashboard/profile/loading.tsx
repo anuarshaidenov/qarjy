ac095bffbc34c370941ee38f8768788769eb2c57
anuar <anuarshaidenov@gmail.com>
2025-08-05 15:30:27 +0500

    Add skeleton ui for profile page

--- app/[locale]/dashboard/profile/loading.tsx
+++ app/[locale]/dashboard/profile/loading.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { Loader, Loader2 } from "lucide-react";
 import React from "react";
 
@@ -6,7 +7,8 @@ type Props = {};
 const ProfileLoadingState = (props: Props) => {
   return (
     <section className="py-8 px-4 container flex flex-col gap-4">
-      <Loader2 className="size-10 animate-spin text-muted-foreground mb-4" />
+      <Skeleton className="w-full max-w-[300px] h-[39.59px] mb-8" />
+      <Skeleton className="w-full max-w-[385px] h-[342px]" />
     </section>
   );
 };
